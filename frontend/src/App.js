65e50157365d09edaef7721c664d538123a46f5a
almujahedmarah <marah.almujahed.ruh.js@tuwaiq.edu.sa>
2021-12-12 20:55:14 +0300

    nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import User from './comp/user/L/S'
 import './App.css';
+import Nav from './comp/pages/Nav'
+import Jadore from "./comp/pages/Jadore"
+import MissD from "./comp/pages/MissD"
+import Joy from "./comp/pages/Joy"
+import Cart from './comp/pages/Cart'
+import User from './comp/user/User'
+
+import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
+      <Nav />
       
+      < Routes>
+         {/* <Route exact="true" path="/" element={<Hhh />}></Route> */}
+         <Route path="/Jadore" element={<Jadore />}></Route>
+         <Route path="/MissD" element={<MissD />}></Route>
+         <Route path="/Joy" element={<Joy />}></Route>
+         <Route path="/Cart" element={<Cart />}></Route>
+         <Route path="/User" element={<User />}></Route>
+        </Routes>
+    
     </div>
   )
 }
---
+++ frontend/src/comp/pages/Nav.js
@@ -0,0 +1,26 @@
+import react from "react";
+import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
+import Home from './Home'
+import Jadore from "./Jadore"
+import MissD from "./MissD"
+import Joy from "./Joy"
+import Cart from './Cart'
+import User from '../user/User'
+
+// import './App.css'
+export default function Nav() {
+    return (
+        <div className="nav">
+            <ul>
+                < Link className="link"  to="/">home</Link>
+                < Link  className="link"  to="/Jadore">Jadore</Link>
+                < Link  className="link"  to="/MissD">Miss Dior</Link>
+                < Link  className="link"  to="/Joy">Joy</Link>
+                < Link  className="link"  to="/Cart">Cart</Link>
+                < Link  className="link"  to="/User">User</Link>
+            </ul>
+      
+    
+        </div>
+    )
+}
