9fca039494ba2a0adefa78d42c189278625d6813
almujahedmarah <marah.almujahed.ruh.js@tuwaiq.edu.sa>
2021-12-12 13:58:46 +0300

    footer

---
+++ frontend/src/comp/pages/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+     <div className='footer'>
+            <hr></hr>
+            <div className='footere'>
+        <div className='logoF'>
+         <h1>Dior</h1>
+         <p><b>OUR BOUTIQUES</b></p>
+         <p>Christian Dior Couture Boutiques</p>
+         <p>Parfums Christian Dior Boutiques</p>
+         </div>
+         <div>
+         <p><b> THE HOUSE OF DIOR</b></p>
+         <p>General Sales Conditions</p>
+         <p>Christian Dior Couture</p>
+         <p>Christian Dior Couture</p>
+         <p>Christian Dior Couture</p>
+         </div>
+        <div >
+            <img className='footerimg' src="https://i.pinimg.com/564x/9b/39/7e/9b397e09f70d7d218153daf05657cc45.jpg"/>
+        </div>
+        </div>
+        </div>
+    )
+}
---
+++ frontend/src/comp/user/L/S.js
@@ -0,0 +1,103 @@
+import React, {useState} from "react";
+// import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
+
+export default function User() {
+
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+    const [name,setName] =useState("")
+    const [user, setUser] = useState([])
+//============================LOGIN-=====================================================================================
+      //  const navigate= useNavigate()
+      const [userg ,setUserg] = useState('')
+      const [emailg ,setEmailg] = useState([])
+      const [passg ,setPassg] = useState([])
+
+      const handlesubg = (e) =>{
+        e.preventDefault()
+        axios.post(" http://localhost:3001/user/login", {email:emailg, password:passg})
+        .then( (response) => {
+          console.log(response.data);
+          localStorage.setItem("token", response.data.token)
+          localStorage.setItem("id", response.data.id)
+
+        setUserg(response.data)
+        //   navigate("/")
+        //   alert("Hello!! welcom");
+        })
+    }
+//===========SINGUP==============================================================================================
+
+  
+
+    const handlesub =()=>{
+        axios.post(" http://localhost:3001/user/signup", {email:email, password:pass, name:name})
+        .then( (res) =>{
+            console.log(res.data);
+            setUser([...user,res.data])
+        })
+            //   navigate("/")
+            //   alert("Hello!! welcom");
+    }
+
+// =============================================================================================================
+    return (
+        <div>
+            <Parallax pages={2} style={{ top: '0', left: '0', backgroundColor: "rgba(223, 186, 186, 0.877)"}}>
+
+<ParallaxLayer
+  offset={0}
+  speed={2.5}
+  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: "rgba(223, 186, 186, 0.877)" }}>
+            <div className="sup-main">
+            <div className="input">
+                <h2>Singup</h2>
+                <div className="oo">
+            <div >
+            <input type="text" placeholder="name" className="name" onChange={(e)=>setName(e.target.value)} />
+            <div className="ss-in"> 
+            <input type="email" placeholder="email" className="name" onChange={(e) =>setEmail(e.target.value) } />
+            </div>
+            </div>
+            <div className="s-input">
+            <input type="password" placeholder="password" className="name" onChange={(e)=>setPass(e.target.value)}/>
+            </div>
+            </div>
+            <button className="singup" type="submit" onClick={(e) =>handlesub(e)}>Singup</button>
+            </div>
+            </div>
+</ParallaxLayer>
+
+<ParallaxLayer className='ii' offset={1} speed={2} style={{ backgroundColor: "rgba(61, 47, 47, 0.877)" }} />
+
+<ParallaxLayer
+  offset={1}
+  speed={0.5}
+  style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }}>
+            <div className="sup-main">
+            <div className="input">
+                <h2>Login</h2>
+                <div className="oo">
+            <div >
+            <input type="email" placeholder="email" className="name" onChange={(e)=>setEmailg(e.target.value)} />
+            </div>
+            <div className="s-input">
+            <input type="password" placeholder="password" className="name" onChange={(e)=>setPassg(e.target.value)} />
+            </div>
+            </div>
+            <button className="singup" type="submit" onClick={(e => handlesubg(e))} >Login</button>
+            </div>
+            </div>
+
+</ParallaxLayer>
+</Parallax>
+        </div>
+    )
+}
