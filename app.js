d2411e3820e8120e3915eeb37692afd3b5ed6f45
almujahedmarah <marah.almujahed.ruh.js@tuwaiq.edu.sa>
2021-12-29 10:39:09 +0300

    v28

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post("/payment", (req, res) => {
 });
 
 
-app.use('/', express.static(path.join(__dirname, '/client/build')));
+app.use('/', express.static(path.join(__dirname, '/frontend/build')));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
